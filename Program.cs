20330c31cecbed5d0aef85aa8527310c27dbcb77
schumpitazi123 <schumpitazi@mail.usf.edu>
2020-01-30 21:59:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserInputSofiaChumpitazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.WriteLine("Hi, this is Sofia Chumpitazi");
+            //Instructions for users to type and press Enter key
+            Console.WriteLine("Type something here... When you are finished, press Enter! ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("The user typed: ");
+            Console.WriteLine(userInput);
+
+            Console.ReadKey();
+        }
+
+       
+        
+    }
+}
